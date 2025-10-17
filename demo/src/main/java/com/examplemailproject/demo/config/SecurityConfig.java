3dbbed136add3f2c0d3df77f187531e88b7c5693
Chheche <rafael.barreto.fr@gmail.com>
2025-10-17 14:32:00 +0200

    Ajout du fichier de sécurité spring

---
+++ demo/src/main/java/com/examplemailproject/demo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.examplemailproject.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers
+                .frameOptions(frame -> frame.sameOrigin())
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+}
+
+//.logout(logout -> logout.disable());
