b891b92ee99800986e3a980f469127cb0ced77e9
Chheche <rafael.barreto.fr@gmail.com>
2025-10-17 14:35:32 +0200

    Ajout des Dao

---
+++ demo/src/main/java/com/examplemailproject/demo/model/Email.java
@@ -0,0 +1,51 @@
+package com.examplemailproject.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "emails")
+@Data
+public class Email {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String job;
+    private String company;
+    private String date;
+    private String etat;
+
+    private String userEmail;
+    private String source;
+
+
+    public Email() {}
+
+    public Email(String job, String company, String date, String etat) {
+        this.job = job;
+        this.company = company;
+        this.date = date;
+        this.etat = etat;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getJob() { return job; }
+    public void setJob(String job) { this.job = job; }
+
+    public String getCompany() { return company; }
+    public void setCompany(String company) { this.company = company; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getEtat() { return etat; }
+    public void setEtat(String etat) { this.etat = etat; }
+
+    public String getUserEmail() { return userEmail; }
+    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
+
+    public String getSource() { return source; }
+    public void setSource(String source) { this.source = source; }
+}
